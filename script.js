45d24b1ee21b28e93e5d69124fe7fe29a6ad8ccc
Sai <medavarapusaikeerthan@gmail.com>
2025-04-27 23:37:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", function() {
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+});
