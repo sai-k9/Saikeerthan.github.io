85ccad23856ae85e6fd8cce7f30bddf9ab7668ae
Sai <medavarapusaikeerthan@gmail.com>
2025-04-28 00:17:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Scroll Reveal Animations
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", function() {
@@ -12,4 +13,10 @@ window.addEventListener("scroll", function() {
             reveals[i].classList.remove("active");
         }
     }
+});
+
+// Loader Animation
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loader");
+    loader.style.display = "none";
 });
