7b3fb5830b9a18c5e27672521f968f44caf13c39
Sai <medavarapusaikeerthan@gmail.com>
2025-04-28 00:33:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Scroll animation
 const reveals = document.querySelectorAll(".reveal");
+
 window.addEventListener("scroll", function() {
     for (let i = 0; i < reveals.length; i++) {
         const windowHeight = window.innerHeight;
         const elementTop = reveals[i].getBoundingClientRect().top;
         const elementVisible = 150;
+
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
         } else {
@@ -11,6 +14,8 @@ window.addEventListener("scroll", function() {
         }
     }
 });
+
+// Hide loader
 window.addEventListener("load", function() {
     const loader = document.getElementById("loader");
     loader.style.display = "none";
