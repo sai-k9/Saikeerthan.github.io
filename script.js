d238c93d60bc6a5efa74bb6c9b2aa5d9bf581ca9
Sai <medavarapusaikeerthan@gmail.com>
2025-04-28 00:25:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll(".reveal");
+window.addEventListener("scroll", function() {
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+});
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loader");
+    loader.style.display = "none";
+});
